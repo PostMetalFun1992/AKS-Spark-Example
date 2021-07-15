8a5df6b33b9495ae21ec67221e132ca4f797849f
Kabanov Kirill <Kirill_Kabanov@epam.com>
2021-07-15 13:09:28 +0700

    Reformatted source files

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup
 
 setup(
-    name='sparkbasics',
-    version='1.0.0',
-    description='BDCC Pyspark Basics project',
-    py_moudles=['__main__'],
-    packages=['src.main.python'],
-    zip_safe=False
+    name="sparkbasics",
+    version="1.0.0",
+    description="BDCC Pyspark Basics project",
+    py_moudles=["__main__"],
+    packages=["src.main.python"],
+    zip_safe=False,
 )
