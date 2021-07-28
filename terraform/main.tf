44bcfb0a9f133fc7b92d6334e437c16e86730fc2
Kabanov Kirill <Kirill_Kabanov@epam.com>
2021-07-28 18:40:58 +0700

    Added aks declaration

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,3 +61,40 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Basic"
   admin_enabled       = false
 }
+
+resource "azurerm_kubernetes_cluster" "bdcc" {
+  depends_on = [
+    azurerm_resource_group.bdcc
+  ]
+
+  name                = "aks-${var.ENV}-${var.LOCATION}"
+  location            = azurerm_resource_group.bdcc.location
+  resource_group_name = azurerm_resource_group.bdcc.name
+  dns_prefix          = "bdcc${var.ENV}"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    region = var.BDCC_REGION
+    env    = var.ENV
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.bdcc.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.bdcc.kube_config_raw
+  sensitive = true
+}
+
